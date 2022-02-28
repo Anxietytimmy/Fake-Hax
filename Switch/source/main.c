c7434a25472f2da38b5b3e2b13c5cf663b7887c4
Anxietytimmy <71737812+Anxietytimmy@users.noreply.github.com>
2022-02-28 09:10:56 -0800

    The first switch support
    Lobotomized code from the looping test repository

---
+++ Switch/source/main.c
@@ -0,0 +1,44 @@
+// Include the nessecary libs 
+#include <switch.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h> 
+#include <unistd.h>
+
+// Main program entrypoint
+int main(int argc, char* argv[])
+{
+    // This example uses a text console, as a simple way to output text to the screen.
+    consoleInit(NULL);
+
+    // Configure our supported input layout: a single player with standard controller styles
+    padConfigureInput(1, HidNpadStyleSet_NpadStandard);
+
+    // Initialize the default gamepad (which reads handheld mode inputs as well as the first connected controller)
+    PadState pad;
+    padInitializeDefault(&pad);
+    // print A int text
+    printf("Press A to start looping\n");            
+    
+    // Main loop
+    while (appletMainLoop())
+    {
+        // Scan the gamepad. This should be done once for each frame
+        padUpdate(&pad);
+
+        // padGetButtonsDown returns the set of buttons that have been
+        // newly pressed in this frame compared to the previous one
+        u64 kDown = padGetButtonsDown(&pad);
+
+
+        if (kDown & HidNpadButton_Plus)
+            break; // break in order to return to hbmenu
+
+        // Update the console, sending a new frame to the display
+        consoleUpdate(NULL);
+    }
+
+    // Deinitialize and clean up resources used by the console (important!)
+    consoleExit(NULL);
+    return 0;
+}
